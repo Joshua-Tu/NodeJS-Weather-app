3c551d4f713bdf3f4c4a0a97d8953bf7929d571e
JoshTu <joshua.tu@primexconnect.com>
2020-07-10 18:30:18 +1000

    Add weather and location content fetched from backend to the landing page onece searched

--- public/js/app.js
+++ public/js/app.js
@@ -2,20 +2,26 @@ console.log('Client side javascript file is loaded');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', evt => {
   evt.preventDefault();
 
   const location = search.value;
 
+  messageOne.innerHTML = 'Loading...';
+  messageTwo.innerHTML = '';
+
   fetch(`http://localhost:3000/weather?address=${location}`)
   .then( res => {
     res.json().then(data => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.innerHTML = data.error;
+        messageTwo.innerHTML = '';
       } else {
-        console.log(data.address);
-        console.log(data.forecast);
+        messageOne.innerHTML = 'Address: ' + data.forecast.location;
+        messageTwo.innerHTML = 'Weather condition: ' + data.forecast.temperature + ', ' + data.forecast.weather;
       }
     })
   });
