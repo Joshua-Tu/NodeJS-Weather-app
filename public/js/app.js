faa13bfa484ed3a4ebb06ed66311d6d9912bf4bb
JoshTu <joshua.tu@primexconnect.com>
2020-07-10 18:01:11 +1000

    Add search function to search form so that it can fetch data from the backend

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,5 @@
 console.log('Client side javascript file is loaded');
 
-fetch('http://localhost:3000/weather?address=!')
-.then( res => {
-  res.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.address);
-      console.log(data.forecast);
-    }
-  })
-});
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
@@ -20,5 +8,15 @@ weatherForm.addEventListener('submit', evt => {
 
   const location = search.value;
 
-  console.log(location);
+  fetch(`http://localhost:3000/weather?address=${location}`)
+  .then( res => {
+    res.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.address);
+        console.log(data.forecast);
+      }
+    })
+  });
 })
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request');
 const geocode = (address, cb) =>  {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(address)}.json?access_token=pk.eyJ1Ijoiam9zaHVhdHUiLCJhIjoiY2s2NTIxd3pkMG5jajNmbHdmZmxhOTZ6MiJ9.3zdz7RsqcjUDM7KSV-3j0g&limit=1`;
   // {body: {features}} 就是把response给提前destructuring了
-  request({url, json: true}, (err, {body: {features}}) => {
+  request({url, json: true}, (err, {body: {features = {}}} = {}) => {
     if (err) {
       cb('Unable to connect to location services!', undefined);
     } else if (features.length === 0) {
