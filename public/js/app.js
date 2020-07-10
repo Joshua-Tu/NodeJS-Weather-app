040c57a7c32f6391fd9bb60d239b58323c97dcf1
JoshTu <joshua.tu@primexconnect.com>
2020-07-10 17:30:02 +1000

    Delete the demo code in public js app.js file

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,5 @@
 console.log('Client side javascript file is loaded');
 
-fetch('http://puzzle.mead.io/puzzle').then(res => {
-  res.json().then(data => {
-    console.log(data);
-  });
-});
-
 fetch('http://localhost:3000/weather?address=!')
 .then( res => {
   res.json().then(data => {
